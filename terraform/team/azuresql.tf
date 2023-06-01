0a055a3a1c89d26bb15d3ee6c34dba250a0a00d4
Clémence Lesné <clemence@lesne.pro>
2023-06-01 15:43:44 +0200

    Fix: Terraform deprecated resources

--- terraform/team/azuresql.tf
+++ terraform/team/azuresql.tf
@@ -1,17 +1,15 @@
-resource "azurerm_sql_server" "gamesqlserver" {
+resource "azurerm_mssql_server" "gamesqlserver" {
   name                         = "game-sqlserver-${local.hash_suffix}"
   resource_group_name          = azurerm_resource_group.aksrg.name
   location                     = azurerm_resource_group.aksrg.location
   version                      = "12.0"
   administrator_login          = "gamedbadministrator"
   administrator_login_password = "${random_string.azuresqldbpw.result}"
-
-
 }
 
 resource "azurerm_mssql_database" "gamedb" {
   name           = "gamedb"
-  server_id      = azurerm_sql_server.gamesqlserver.id
+  server_id      = azurerm_mssql_server.gamesqlserver.id
   max_size_gb    = 32
   sku_name       = "GP_Gen5_8"
 }
@@ -27,25 +25,25 @@ output "SQL_DATABASE_NAME" {
 
 output "SQL_SERVER_NAME" {
   description = "Server name of the Azure SQL Database created."
-  value       = azurerm_sql_server.gamesqlserver.name
+  value       = azurerm_mssql_server.gamesqlserver.name
 }
 
 output "SQL_SERVER_LOCATION" {
   description = "Location of the Azure SQL Database created."
-  value       = azurerm_sql_server.gamesqlserver.location
+  value       = azurerm_mssql_server.gamesqlserver.location
 }
 
 output "SQL_SERVER_VERSION" {
   description = "Version the Azure SQL Database created."
-  value       = azurerm_sql_server.gamesqlserver.version
+  value       = azurerm_mssql_server.gamesqlserver.version
 }
 
 output "SQL_FQDN" {
   description = "Fully Qualified Domain Name (FQDN) of the Azure SQL Database created."
-  value       = azurerm_sql_server.gamesqlserver.fully_qualified_domain_name
+  value       = azurerm_mssql_server.gamesqlserver.fully_qualified_domain_name
 }
 
 output "SQL_CONNECTION_STRING" {
   description = "Connection string for the Azure SQL Database created."
-  value       = nonsensitive("Server=tcp:${azurerm_sql_server.gamesqlserver.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.gamedb.name};Persist Security Info=False;User ID=${azurerm_sql_server.gamesqlserver.administrator_login};Password=${azurerm_sql_server.gamesqlserver.administrator_login_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;")
+  value       = nonsensitive("Server=tcp:${azurerm_mssql_server.gamesqlserver.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.gamedb.name};Persist Security Info=False;User ID=${azurerm_mssql_server.gamesqlserver.administrator_login};Password=${azurerm_mssql_server.gamesqlserver.administrator_login_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;")
 }
